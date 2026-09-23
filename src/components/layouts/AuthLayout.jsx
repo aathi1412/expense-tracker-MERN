f9e08287c963680ad933bce50e33b1ff686ab11a
Aathi <aathithirunav@gmail.com>
2026-09-23 23:32:43 +0530

    feat(layout): Add AuthLayout component
    - Provides a layout for authentication pages with a background image and styled containers

---
+++ src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,23 @@
+import expense3 from '../../assets/images/expense3.png';
+
+function AuthLayout({ children }){
+
+    console.log(expense3);
+    return (
+        <div>
+            <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+                <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+                {children}
+            </div>
+
+            <div className=" md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+                <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5"></div>
+                <div className="w-48 h-56 rounded-[40px] border-20 border-fuchsia-600 absolute top-[30%] -right-10"></div>
+                <div className="w-48 h-48 rounded-[40px] bg-violet-600 absolute -bottom-7 -left-5"></div>
+
+                <img src={expense3} alt="expense-image"/>
+            </div>
+        </div>
+    )
+}
+export default AuthLayout
